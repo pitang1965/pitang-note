290e4255874799b15e32ab9c8803230baed09b20
pitang1965 <pitang1965@gmail.com>
2023-01-15 11:26:50 +0900

    💄 Add top margin

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -9,7 +9,7 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <a href="{{ route('notes.create') }}" class="btn-link btn-lg mb-2">+ メモを追加</a>
       @forelse ($notes as $note)
-      <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+      <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg mt-6">
         <h2 class="font-bold text-2xl">
           {{ $note->title }}
         </h2>
