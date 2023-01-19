3e5622bf0097ecd22f453d4eb04f71af5a638667
pitang1965 <pitang1965@gmail.com>
2023-01-19 22:26:03 +0900

    💄 Add Created and Updated

--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -7,6 +7,14 @@
 
   <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="flex">
+        <p class="opacity-70">
+          <strong>作成日： </strong> {{ $note->created_at->diffForHumans() }}
+        </p>
+        <p class="opacity-70 ml-8">
+          <strong>更新日： </strong> {{ $note->updated_at->diffForHumans() }}
+        </p>
+      </div>
       <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg mt-6">
         <h2 class="font-bold text-2xl">
           {{ $note->title }}
