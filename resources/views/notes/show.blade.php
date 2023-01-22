f53a6787c8a1c65a2f546dcd89dacad82e7d7ec2
pitang1965 <pitang1965@gmail.com>
2023-01-22 22:52:29 +0900

    ✨ Add a destroy method to delete a  note

--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -15,6 +15,11 @@
           <strong>更新日： </strong> {{ $note->updated_at->diffForHumans() }}
         </p>
         <a href="{{ route('notes.edit', $note) }}" class="btn-link ml-auto">メモを編集</a>
+        <form action="{{ route('notes.destroy', $note) }}" method="post">
+          @method('delete')
+          @csrf
+          <button type="submit" class="btn btn-danger ml-4" onClick="return confirm('このメモを本当に削除しますか？')">メモを削除</button>
+        </form>
       </div>
       <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg mt-6">
         <h2 class="font-bold text-2xl">
